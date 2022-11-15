1985fb6cc37a1bddde1613e8c50b26f322561bec
rajendra <rajendran.scm@gmail.com>
2022-11-16 02:08:59 +0530

    Jenkins installation on EC2

--- 1.configure_backend_remote_state/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "tfremotetest"
-    region         = "us-east-1"
-    key            = "backend.tfstate"
-    dynamodb_table = "tfstatelock"
-  }
-}
--- 2.Modules/Use_Modules/modules/ec2/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "app_server" {
-  ami           = "ami-0c4e4b4eb2e11d1d4"
-  instance_type = "t3.micro"
-  subnet_id     = "subnet-081e02cfef8d11dff"
-  tags = {
-    Name = "WayneCorp"
-  }
-}
